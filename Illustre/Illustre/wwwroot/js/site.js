9c2b3206cf514dd5687865340e6c0bb0f8491ac4
AnnBazille <dr.anna.625@gmail.com>
2022-06-06 14:17:19 +0300

    Refactor Add Image menu

--- Illustre/Illustre/wwwroot/js/site.js
+++ Illustre/Illustre/wwwroot/js/site.js
@@ -1,4 +1,14 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+(function () {
+    $("#illustre-image-file").change(function () {
+        if (typeof (FileReader) != "undefined") {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $("#illustre-image-preview").css('background-image', 'url(' + e.target.result + ')');
+            }
+            reader.readAsDataURL($(this)[0].files[0]);
+        }
+    });
+})();
