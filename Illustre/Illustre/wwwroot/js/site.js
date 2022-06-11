58405a1146766ebeacc4ffe04f31ae81ef7ee458
AnnBazille <dr.anna.625@gmail.com>
2022-06-11 22:45:18 +0300

    Add preview for loaded images

--- Illustre/Illustre/wwwroot/js/site.js
+++ Illustre/Illustre/wwwroot/js/site.js
@@ -7,6 +7,7 @@
             var reader = new FileReader();
             reader.onload = function (e) {
                 $("#illustre-image-preview").css('background-image', 'url(' + e.target.result + ')');
+                $("#illustre-add-image-preview").attr("src", e.target.result);
             }
             reader.readAsDataURL($(this)[0].files[0]);
         }
