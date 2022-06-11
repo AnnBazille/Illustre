f32b9939b07ca64c8b4040005afad992c583875c
AnnBazille <dr.anna.625@gmail.com>
2022-06-12 02:26:02 +0300

    Move modal preview to partial view

---
+++ Illustre/Illustre/Models/ModalImagePreviewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Illustre.Models;
+
+public class ModalImagePreviewModel
+{
+    private string _modalId;
+
+    public string ModalId
+    {
+        get => "illustre-modal-" + _modalId;
+        set => _modalId = value;
+    }
+
+    public string Title { get; set; }
+
+    public string Image { get; set; }
+
+    public string Hashtag
+    {
+        get => "#" + ModalId;
+    }
+}
